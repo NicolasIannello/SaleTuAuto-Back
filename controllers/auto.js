a89272f756da9d61fbfd87d6f8f5b3d56c106b5b
NicolasIannello <nicolas.iannello99@gmail.com>
2025-01-08 14:45:32 -0300

    get auto data + imgs

--- controllers/auto.js
+++ controllers/auto.js
@@ -1,5 +1,8 @@
 const Auto = require("../models/auto");
+const Imagen = require("../models/imagen");
 const { response }=require('express');
+const path=require('path');
+const fs=require('fs');
 
 const getAutos= async(req,res = response) =>{
     const desde= parseInt(req.query.desde) || 0;
@@ -28,4 +31,45 @@ const getAutos= async(req,res = response) =>{
     });
 };
 
-module.exports={ getAutos }
+const auto= async(req,res = response) =>{    
+    const auto = await Auto.aggregate([
+        { "$match": { uuid:req.body.uuid } },
+        { $project: { __v: 0, '_id':0, 'uuid':0 } },
+        { $lookup: {
+            from: "imagens",
+            localField: "uuid_auto",
+            foreignField: "uuid",
+            "pipeline": [ { "$sort" : { "orden" : 1 } } ],
+            as: "img"
+        } },
+        { $project: { __v: 0, "img.__v": 0, "img._id": 0, "img.uuid_auto": 0, } },
+    ]).collation({locale: 'en'})
+
+    res.json({
+        ok:true,
+        auto
+    });
+};
+
+const getArchivo= async(req,res = response) =>{
+    const img=req.query.img;
+    const tipo=req.query.tipo;
+    let pathImg;
+    if(tipo=='autos') {
+        const imagenesDB= await Imagen.find({img});
+        if(imagenesDB.length>0){
+            pathImg=pathImg= path.join( __dirname, '../files/autos/'+imagenesDB[0].img);
+        }else{
+            pathImg=pathImg= path.join( __dirname, '../files/autos/'+imagenesDB.img);
+        }
+    }
+
+    if(fs.existsSync(pathImg)){
+        res.sendFile(pathImg);
+    }else{
+        const pathImg= path.join( __dirname, '../files/no-img.jpg');
+        res.sendFile(pathImg);
+    }
+};
+
+module.exports={ getAutos, auto, getArchivo }
--- routes/autos.js
+++ routes/autos.js
@@ -1,8 +1,21 @@
 const { Router }=require('express');
-const { getAutos } = require('../controllers/auto');
+const { check }=require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { getAutos, auto, getArchivo } = require('../controllers/auto');
 
 const router=Router();
 
 router.post('/autos', [], getAutos);
+
+router.post('/auto', [
+    check('uuid','el campo es obligatorio').not().isEmpty(),
+    check('token','el campo es obligatorio').not().isEmpty(),
+    check('tipo','el campo es obligatorio').not().isEmpty(),
+    validarCampos,
+    validarJWT
+], auto);
+
+router.get('/img', getArchivo);
 
 module.exports=router;
