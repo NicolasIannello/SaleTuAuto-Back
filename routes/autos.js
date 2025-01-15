23e9e6569e98eb6056771f06ffccac28ebe54dbe
NicolasIannello <nicolas.iannello99@gmail.com>
2025-01-15 13:38:22 -0300

    -check vars

--- routes/autos.js
+++ routes/autos.js
@@ -1,7 +1,6 @@
 const { Router }=require('express');
 const { check }=require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
 const { getAutos, auto, getArchivo, datos } = require('../controllers/auto');
 
 const router=Router();
@@ -10,10 +9,7 @@ router.post('/autos', [], getAutos);
 
 router.post('/auto', [
     check('uuid','el campo es obligatorio').not().isEmpty(),
-    check('token','el campo es obligatorio').not().isEmpty(),
-    check('tipo','el campo es obligatorio').not().isEmpty(),
     validarCampos,
-    validarJWT
 ], auto);
 
 router.get('/img', getArchivo);
