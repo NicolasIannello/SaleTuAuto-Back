2f791396c794ffd131024bee7072666bb72d7207
NicolasIannello <nicolas.iannello99@gmail.com>
2025-01-15 13:46:53 -0300

    mail contacnto

--- sta.js
+++ sta.js
@@ -13,6 +13,7 @@ dbConnection();
 
 app.use('/sta/admin', require('./routes/admins'));
 app.use('/sta/auto', require('./routes/autos'));
+app.use('/sta/usuarios', require('./routes/usuarios'));
 
 app.listen( process.env.PORT, () =>{
     console.log('Iniciando');
