8a0a24e3da42c1f7f6ac596c138d13f1c0eec34c
NicolasIannello <nicolas.iannello99@gmail.com>
2025-01-17 14:46:29 -0300

    --mail

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,42 +1,42 @@
-const { response }=require('express');
-const nodemailer = require("nodemailer");
+// const { response }=require('express');
+// const nodemailer = require("nodemailer");
 
-const mailContacto= async(req,res=response)=>{    
-    const {mensaje,mensaje2,asunto,nombre_apellido,mail} = req.body
-    const transporter = nodemailer.createTransport({
-        maxConnections: 1,
-        pool: true,
-        host: process.env.MSERVICE,
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'contacto@gruppodf.com.ar',
-            pass: process.env.MPASS
-        }
-    });
+// const mailContacto= async(req,res=response)=>{    
+//     const {mensaje,mensaje2,asunto,nombre_apellido,mail} = req.body
+//     const transporter = nodemailer.createTransport({
+//         maxConnections: 1,
+//         pool: true,
+//         host: process.env.MSERVICE,
+//         port: 465,
+//         secure: true,
+//         auth: {
+//             user: 'contacto@gruppodf.com.ar',
+//             pass: process.env.MPASS
+//         }
+//     });
 
-    let msg = "Nombre y apellido: "+nombre_apellido+"\n"+"E-Mail: "+mail+"\n"+"Mensaje:\n"+mensaje
-    let msgHTML = "Nombre y apellido: "+nombre_apellido+"<br><br>"+"E-Mail: "+mail+"<br><br>"+"Mensaje:<br>"+mensaje2
+//     let msg = "Nombre y apellido: "+nombre_apellido+"\n"+"E-Mail: "+mail+"\n"+"Mensaje:\n"+mensaje
+//     let msgHTML = "Nombre y apellido: "+nombre_apellido+"<br><br>"+"E-Mail: "+mail+"<br><br>"+"Mensaje:<br>"+mensaje2
 
-    transporter.sendMail({
-        from: '"Gruppo DF Subastas" <contacto@gruppodf.com.ar>',
-        to: 'contacto@gruppodf.com.ar',
-        subject: "SALE TU AUTO Formulario contacto: "+asunto,
-        text: msg,
-        html: msgHTML,
-    }, function(error, info){
-        if (error) {
-            console.log(error);
-            return res.status(400).json({
-                ok:false,
-                msg:'error'
-            });        
-        }
-    });
+//     transporter.sendMail({
+//         from: '"Gruppo DF Subastas" <contacto@gruppodf.com.ar>',
+//         to: 'contacto@gruppodf.com.ar',
+//         subject: "SALE TU AUTO Formulario contacto: "+asunto,
+//         text: msg,
+//         html: msgHTML,
+//     }, function(error, info){
+//         if (error) {
+//             console.log(error);
+//             return res.status(400).json({
+//                 ok:false,
+//                 msg:'error'
+//             });        
+//         }
+//     });
     
-    res.json({
-        ok:true,
-    })
-};
+//     res.json({
+//         ok:true,
+//     })
+// };
 
-module.exports={ mailContacto }
+// module.exports={ mailContacto }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,13 +1,13 @@
-const { Router }=require('express');
-const { check }=require('express-validator');
-const { validarCampos } = require('../middlewares/validar-campos');
-const { mailContacto } = require('../controllers/usuario');
+// const { Router }=require('express');
+// const { check }=require('express-validator');
+// const { validarCampos } = require('../middlewares/validar-campos');
+// const { mailContacto } = require('../controllers/usuario');
 
-const router=Router();
+// const router=Router();
 
-router.post('/mailContacto', [
-    check('uuid','el campo es obligatorio').not().isEmpty(),
-    validarCampos,
-], mailContacto);
+// router.post('/mailContacto', [
+//     check('uuid','el campo es obligatorio').not().isEmpty(),
+//     validarCampos,
+// ], mailContacto);
 
-module.exports=router;
+// module.exports=router;
