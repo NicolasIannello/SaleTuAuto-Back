1608344827e3f784a07180a4656764366d7b2c90
NicolasIannello <nicolas.iannello99@gmail.com>
2025-02-17 12:57:46 -0300

    usuarios / mailContacto

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,13 +1,14 @@
-// const { Router }=require('express');
-// const { check }=require('express-validator');
-// const { validarCampos } = require('../middlewares/validar-campos');
-// const { mailContacto } = require('../controllers/usuario');
+const { Router }=require('express');
+const { check }=require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { mailContacto } = require('../controllers/usuario');
 
-// const router=Router();
+const router=Router();
 
-// router.post('/mailContacto', [
-//     check('uuid','el campo es obligatorio').not().isEmpty(),
-//     validarCampos,
-// ], mailContacto);
+router.post('/mailContacto', [
+    check('nomapel','el campo es obligatorio').not().isEmpty(),
+    check('telefono','Telefono invalido').isMobilePhone(),
+    validarCampos,
+], mailContacto);
 
-// module.exports=router;
+module.exports=router;
